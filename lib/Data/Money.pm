3ec2ad14f954c9d075470e4d292f3d52a2bde5f7
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-03-05 03:34:45 +0000

    - Added key 'provides' to the Makefile.PL script. - Added test script t/meta-json.t and t/meta-yml.t.

--- lib/Data/Money.pm
+++ lib/Data/Money.pm
@@ -1,6 +1,6 @@
 package Data::Money;
 
-$Data::Money::VERSION   = '0.16';
+$Data::Money::VERSION   = '0.17';
 $Data::Money::AUTHORITY = 'cpan:GPHAT';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Data::Money - Money/currency with formatting and overloading.
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =cut
 
